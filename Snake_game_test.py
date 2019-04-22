67e789e16bc8ce01f646b38c341475c9a3bac631
learnsas <aniljadhav2996@gmail.com>
2019-04-22 13:14:58 +0530

    Mysql database connections

--- Snake_game_test.py
+++ Snake_game_test.py
@@ -1,1 +1,11 @@
-print('Hellow world')
+import mysql.connector
+mydb = mysql.connector.connect(host='localhost', user='root', passwd='Admin123', database='python_prac')
+
+mycursor = mydb.cursor()            #point toward perticular databases
+mycursor.execute('select * from student')  #cursor is like box where it store the data
+
+result = mycursor.fetchone()
+print(result)
+
+for i in mycursor:
+    print(i)
